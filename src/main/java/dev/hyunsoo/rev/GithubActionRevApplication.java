29d1d92a5d4b0d848ed9b000ce0342abf5e2c2d6
hyunsoo.lee <hyunsoo.lee@ondotsystems.com>
2022-10-19 15:28:27 +0900

    Initial commit

---
+++ src/main/java/dev/hyunsoo/rev/GithubActionRevApplication.java
@@ -0,0 +1,13 @@
+package dev.hyunsoo.rev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionRevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionRevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/hyunsoo/rev/GithubActionRevApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hyunsoo.rev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionRevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
